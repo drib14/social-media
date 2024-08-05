02b14ac3756da52f05f1bcbfa8d8089f739a9831
drib14 <jhondribramirez7@gmail.com>
2024-08-05 14:22:41 +0800

    cleaning

--- client/src/customRouter/PrivateRouter.js
+++
@@ -1,8 +0,0 @@
-import { Route, Navigate} from 'react-router-dom'
-
-const PrivateRouter = (props) => {
-    const firstLogin = localStorage.getItem('firstLogin')
-    return firstLogin ? <Route {...props} /> : <Navigate to="/" />
-}
-
-export default PrivateRouter
